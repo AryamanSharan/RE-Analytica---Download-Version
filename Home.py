7232e8f954ac3687d52a3de9e9239a39c7822875
Aryaman Sharan <aryamansharan@gmail.com>
2023-06-24 01:00:16 +0530

    hide streamlit logo

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.image("logo@0.1x.png")
 
